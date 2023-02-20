9938247451aab0daa995eea840706cd042d2e792
njambi-winny <winnienjambi04@gmail.com>
2023-02-20 00:31:48 -0800

    ok

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - prints a line
+ * Return: value is 0(success)
+ */
+int main(void)
+{
+printf("with a proper grammer, but the outcome is a piece of art,\n");
+return (0);
+}
+

3be158e6e187f6e27a5d03e050e60a20609601d4
njambi-winny <winnienjambi04@gmail.com>
2023-02-20 04:22:06 -0800

    ok

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - prints a line
+ * Return: value is 0(success)
+ */
+
+int main(void)
+{
+	write("and that piece of art is useful"\"-Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}

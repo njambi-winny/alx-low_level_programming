a846bcacb14bca06c7d609b1e1cfa6a5f1921f0f
njambi-winny <winnienjambi04@gmail.com>
2023-02-20 04:25:52 -0800

    ok

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
  * main - prints a line
- * Return: value is 0(success)
+ * Return: value is 1(success)
  */
 
 int main(void)
 {
-	write("and that piece of art is useful"\"-Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\"-Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
